04e64b8d9e296cd206d68f2721eedab3957a5c56
pr1yamdas <131381963+pr1yamdas@users.noreply.github.com>
2023-11-14 16:10:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import win32com.client
+import webbrowser
+
+def say(text):
+    speak = win32com.client.Dispatch("SAPI.SpVoice")
+    speak.Speak(text)
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language="en-in")
+            print(f"user said: {query}")
+            return query
+        except Exception as e:
+            print(f"Error in speech recognition: {e}")
+            return "error"
