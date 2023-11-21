667108cc37c74de0ba30cf24217660804f81c10a
pr1yamdas <131381963+pr1yamdas@users.noreply.github.com>
2023-11-21 15:32:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,62 @@ import sys
 import subprocess
 import datetime
 import openai
-import os
-from config import apikey  # Make sure to replace 'config' with the actual module containing your API key
+from config import apikey
 import requests
 from config import newsapi
 import pywhatkit
 import re
+import pyautogui
+import time
+import pyttsx3
+
 
 openai.api_key = apikey
 chatStr=""
+current_voice= 0
 
 
+
+
+engine = pyttsx3.init()
+
+def change_voice():
+    voices = engine.getProperty('voices')
+    current_voice = engine.getProperty('voice')
+
+    # Find the index of the current voice
+    current_index = next((i for i, v in enumerate(voices) if v.id == current_voice), None)
+
+    if current_index is not None:
+        # Increment the index to switch to the next voice
+        current_index = (current_index + 1) % len(voices)
+
+        # Set the new voice
+        engine.setProperty('voice', voices[current_index].id)
+
+
+def play_spotify_track(track_name):
+    # Open the web browser and go to the Spotify website
+    webbrowser.open("https://open.spotify.com/search/")
+
+    # Give some time for the website to load
+    time.sleep(8)
+
+    # Use pyautogui to click on the search bar
+    pyautogui.click(x=700, y=100)  # Replace with the actual coordinates of the search bar
+
+    # Type the track name in the search bar
+    pyautogui.write(track_name)
+
+    # Press Enter to initiate the search
+    pyautogui.press('enter')
+
+    time.sleep(4)
+
+    # Click on the first search result (adjust coordinates as needed)
+    pyautogui.moveTo(x=811, y=439)
+
+    pyautogui.click()
 
 def play_video_on_youtube(video_title):
     try:
@@ -29,12 +74,14 @@ def play_video_on_youtube(video_title):
 
 def extract_video_title(query):
     # Use a simple regex to extract text after "play"
-    match = re.search(r'\bplay\b (.+)', query)
+    match = re.search(r'\bvideo of\b (.+)', query)
     if match:
         return match.group(1)
     else:
         return None
 
+
+import requests
 
 def get_news(newsapi):
     news_url = "https://newsapi.org/v2/top-headlines"
@@ -47,16 +94,20 @@ def get_news(newsapi):
         response = requests.get(news_url, params=params)
         news_data = response.json()
 
-        # Extract and display news headlines
+        # Extract and display news headlines with links
         for article in news_data.get("articles", []):
             title = article.get("title", "")
-            print(f"- {title}")
+            url = article.get("url", "")
+            print(f"- {title}: {url}")
 
         if not news_data.get("articles"):
             print("No news available.")
 
     except Exception as e:
         print(f"Error fetching news: {e}")
+
+# Assuming you have the 'newsapi' variable defined with your API key
+
 
 
 def chat(query):
@@ -91,31 +142,12 @@ def chat(query):
 
 
 def say(text):
-    try:
-        speak = win32com.client.Dispatch("SAPI.SpVoice")
-        speak.Speak(text)
-    except Exception as e:
-        print(f"Error in saying: {e}")
+    engine.say(text)
+    engine.runAndWait()
 
-def ai(prompt):
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            temperature=1,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
 
-        gpt_response = response["choices"][0]["text"]
-        print(gpt_response)
-        say(gpt_response)
 
-    except Exception as e:
-        print(f"Error in OpenAI API call: {e}")
-        say("Error in OpenAI API call. Please try again.")
+# ... (existing code)
 
 def take_command():
     r = sr.Recognizer()
@@ -123,10 +155,19 @@ def take_command():
         r.adjust_for_ambient_noise(source)
         r.pause_threshold = 0.8
         try:
-            audio = r.listen(source, timeout=5)  # Set a timeout of 5 seconds for waiting user input
+            audio = r.listen(source, timeout=10)
             query = r.recognize_google(audio, language="en-in")
             print(f"User said: {query}")
+
+
+
             return query.lower()
+
+        except sr.WaitTimeoutError:
+            print("Listening timed out. Please repeat your command.")
+            return ""
+
+
         except sr.UnknownValueError:
             print("Speech Recognition could not understand audio")
             return ""
@@ -134,10 +175,13 @@ def take_command():
             print(f"Could not request results from Google Speech Recognition service; {e}")
             return "error"
 
+# ... (existing code)
+
+
 say("Good morning")
 while True:
     print("Listening..")
-    query = input()
+    query = take_command()
 
 
     sites = {"youtube": "https://youtube.com", "wikipedia": "https://wikipedia.com", "google": "https://google.com", "mail": "https://mail.google.com/mail/u/0/#inbox"}
@@ -171,11 +215,10 @@ while True:
             file_path = r'C:\Users\priya\Downloads\Lil Nas X - Old Town Road (Official Video) ft. Billy Ray Cyrus_r7qovpFAGrQ.mp3'
             subprocess.Popen([file_path], shell=True)
             say("Opening Old Town Road")
-    elif "time" in query:
+    elif "time" in query and "now" in query:
             current_time = datetime.datetime.now().strftime("%H:%M:%S")
             say(f"The time is {current_time}")
-    elif "gpt" in query:
-            ai(prompt=query)
+
     elif any(site in query for site in sites):
             matching_sites = [site for site in sites if site in query]
             say(f"Opening {matching_sites[0]}..")
@@ -184,12 +227,19 @@ while True:
             matching_apps = [app for app in apps if app in query]
             say(f"Opening {matching_apps[0]}..")
             subprocess.Popen([apps[matching_apps[0]]], shell=True)
-    elif "play" in query:
+    elif "play" in query and "video" in query:
             video_title = extract_video_title(query)
             if video_title:
                     play_video_on_youtube(video_title)
             else:
                     say("Please specify a video title after 'play'")
-
+    elif "play" in query and "song" in query:
+            track_name = query.split("song", 1)[1].strip()
+            play_spotify_track(track_name)
+    elif "change voice" in query:
+        change_voice()
+        say("Voice changed.")
+    elif "naughty" in query and "open" in query:
+        webbrowser.open("https://www.pornhub.com/login")
     else:
         chat(query)
